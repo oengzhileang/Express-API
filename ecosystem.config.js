34453de94a8836613ab945dac1ba7f846238ce98
oengzhileang <zhileangCC990@gmail.com>
2024-09-16 09:55:42 +0700

    change path

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: "product-service",
-      script: "server.js",
+      script: "./build/server.js",
       instances: 1,
       autorestart: true,
       watch: false,
